241ee146b59488dc0cf08c7ffb7c74af0ab66d94
Leonardo Padilha <leoprsouza@hotmail.com>
2024-12-08 21:20:59 -0300

    incluindo ms-insurance

---
+++ ms-insurance/ms-insurance/src/main/java/com/leonardo/mvpagi/ms_insurance/MsInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.mvpagi.ms_insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInsuranceApplication.class, args);
+	}
+
+}
---
+++ ms-insurance/ms-insurance/src/test/java/com/leonardo/mvpagi/ms_insurance/MsInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.mvpagi.ms_insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
